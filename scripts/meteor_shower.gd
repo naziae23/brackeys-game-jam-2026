f0cea633857c19ad2638ece1f5ca945384f68533
Vivaan Bhandary <vivaanbhandary@gmail.com>
2026-02-20 22:36:21 -0800

    meteor shower

---
+++ scripts/meteor_shower.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@export var prefab : PackedScene
+@onready var level_manager: Node = $"../LevelManager"
+@onready var timer: Timer = $Timer
+
+@export var vel: Vector2 = Vector2(-100, 100)
+@export var frequency: float = 3.0
+
+func _ready() -> void:
+	timer.wait_time = frequency
+	
+func _on_timer_timeout() -> void:	
+	var camera = get_viewport().get_camera_2d()
+	if not camera: return
+	
+	var viewport_size = get_viewport_rect().size / camera.zoom
+	var cam_center = camera.get_screen_center_position()
+	
+	var left_edge = cam_center.x
+	var right_edge = cam_center.x + (viewport_size.x)
+	var top_edge = cam_center.y - (viewport_size.y / 2.0)
+	
+	var spawn_x = randf_range(left_edge, right_edge)
+	var spawn_y = top_edge - 100
+	
+	var meteor = prefab.instantiate()
+	meteor.level_manager = level_manager
+	meteor.vel = vel
+	
+	get_tree().current_scene.add_child(meteor)
+	meteor.global_position = Vector2(spawn_x, spawn_y)
--- scripts/meteorite.gd
+++ scripts/meteorite.gd
@@ -1,13 +1,12 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
-@onready var level_manager: Node = $"../LevelManager"
+@onready var deathTimer: Timer = $deathTimer
+@onready var byeTimer: Timer = $byeTimer
+
+var level_manager: Node
+var killedPlayer: bool = false
 
 var vel: Vector2 = Vector2(-100, 100)
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,11 +14,26 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	body.die()
-	timer.start()
+	if(body is CharacterBody2D):
+		Engine.time_scale = 0.5
+		body.get_node("CollisionShape2D").queue_free()
+		body.die()
+		killedPlayer = true
+		deathTimer.start()
+		self.visible = false
 
 func _on_timer_timeout() -> void:
 	level_manager.lose_level()
 	Engine.time_scale = 1.0
+	queue_free()
+	
+
+
+func _screen_exited() -> void:
+	if !killedPlayer:
+		byeTimer.start()
+	
+
+
+func _on_bye_timer_timeout() -> void:
+	queue_free()
