fedc3f572c61e4ce80cfce79991e084f69276b98
naziae23 <edroos.nazia@gmail.com>
2026-02-22 00:03:12 -0800

    changed hud to show day instead of score; removed collectable coins from all levels

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -2,13 +2,13 @@ extends Node
 
 signal scene_changed(scene_name, context)
 
-@onready var score_label: Label = $"../HUD/score"
+#@onready var score_label: Label = $"../HUD/score"
 
-var score = 0
-
-func add_point():
-	score += 1
-	score_label.text = str(score)
+#var score = 0
+#
+#func add_point():
+	#score += 1
+	#score_label.text = str(score)
 	
 func win_level():
 	emit_signal("scene_changed", "Level", "")
