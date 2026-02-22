68a50bf54a888f4f298925c860d15fced6364f91
naziae23 <edroos.nazia@gmail.com>
2026-02-22 01:34:08 -0800

    designed antigrav section

---
+++ scripts/end_antigravity.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var gravityModifier = $"../Player/Gravity_modifier"
+@onready var timerLabel = $"../HUD/timer"
+
+func _ready() -> void:
+	gravityModifier.active = true
+	timerLabel.visible = true
+
+func _on_body_entered(body: Node2D) -> void:
+	gravityModifier.active = false
+	timerLabel.visible = false
+	# disable collision monitoring immediately
+	set_deferred("monitoring", false)
+	set_deferred("monitorable", false)
