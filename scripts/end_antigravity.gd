395882915da2d2e5837a00d04a8eed28ea775ac3
naziae23 <edroos.nazia@gmail.com>
2026-02-22 02:10:01 -0800

    completed antigrav portion of final level

--- scripts/end_antigravity.gd
+++ scripts/end_antigravity.gd
@@ -3,10 +3,6 @@ extends Area2D
 @onready var gravityModifier = $"../Player/Gravity_modifier"
 @onready var timerLabel = $"../HUD/timer"
 
-func _ready() -> void:
-	gravityModifier.active = false
-	timerLabel.visible = false
-
 func _on_body_entered(body: Node2D) -> void:
 	gravityModifier.active = false
 	timerLabel.visible = false
